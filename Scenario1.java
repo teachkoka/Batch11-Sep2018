8aa71b5bd77223bc9bdb8c552fcd555ab0d02447
teachkoka <mail2teachkoka@gmail.com>
2018-10-17 20:59:03 -0400

    Exception Scanario

---
+++ Scenario1.java
@@ -0,0 +1,23 @@
+package com.bell.b11.day8.exceptions;
+
+public class Scenario1 {
+
+    public static void main(String[] args){
+        Scenario1 s1 = new Scenario1();
+        s1.method1();
+
+    }
+
+    void method1(){
+        System.out.println("I'm inside method1");
+        method2();
+    }
+
+    void method2(){
+        System.out.println("I'm inside method2");
+
+
+
+
+    }
+}
